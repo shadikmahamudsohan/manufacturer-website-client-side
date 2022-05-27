e88d8516c057e3c91fe4d17e4b62709dba8ef16d
shadikmahamud <shadikmahamudsohan@gmail.com>
2022-05-27 23:30:49 +0600

    redirection user to home if he is not an admin

--- src/Pages/Authentication/RequireAdmin.js
+++ src/Pages/Authentication/RequireAdmin.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useLocation } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase/firebase.init';
 import LoadingSpinner from '../../Shared/LoadingSpinner';
 
@@ -11,6 +12,7 @@ const RequireAdmin = ({ children }) => {
     const { pathname } = useLocation();
     const [isAdmin, setIsAdmin] = useState({});
     const [isLoading, setIsLoading] = useState(true);
+    let navigate = useNavigate();
     useEffect(() => {
         fetch(`https://quiet-basin-59724.herokuapp.com/get-user/${user?.email}`, {
             method: 'GET',
@@ -30,12 +32,13 @@ const RequireAdmin = ({ children }) => {
             });
     }, [user?.email]);
 
+
     if (loading || isLoading) {
         return <LoadingSpinner />;
     }
 
     if (!user || !isAdmin?.admin) {
-        signOut(auth);
+        navigate("/");
         localStorage.setItem('location', pathname);
     }
 
